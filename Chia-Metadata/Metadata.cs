9352d64aca1bb85843ad89855a3771ee2ebea0bd
Julian B <bechti44@googlemail.com>
2023-02-05 21:51:49 +0100

    updated metadata to incorporate a hashset and sictionary of attributes for Filtering Purposes. Please note that this changes the behaviour of the list and might be a breaking change! Use the Add remove and update functions instead.

--- Chia-Metadata/Metadata.cs
+++ Chia-Metadata/Metadata.cs
@@ -13,7 +13,7 @@
             bool Sensitive_Content = false,
             ulong Series_Number = 1,
             ulong Series_Total = 1,
-            List<MetadataAttribute> Attributes = null,
+            MetadataAttribute[] Attributes = null,
             Collection Collection = null)
         {
             format = Format;
@@ -25,15 +25,15 @@
             series_total = Series_Total;
             if (Attributes != null)
             {
-                attributes = Attributes.ToList();
+                attributes = Attributes.ToArray();
             }
             collection = Collection;
         }
         /// <summary>
         /// this constructos is for the json deserializer
         /// </summary>
-        public Metadata() { 
-            attributes = new List<MetadataAttribute> ();
+        public Metadata() {
+            attributes = new MetadataAttribute[] { };
             collection = new Collection();
             format = "CHIP-0007";
         }
@@ -69,11 +69,71 @@
         /// <summary>
         /// the attributes of this nft, eg color, subset, rarity, sharpness, abilities, health, attack, ...
         /// </summary>
-        public List<MetadataAttribute> attributes { get; set; }
+        /// <remarks>
+        /// this array is primairly for Json serialisation / deserialisation. for lookup the hashset or the dictionary is recommended <br/>
+        /// if you need to update the attributes, It is recommended to use the add/remove/update functions.
+        /// </remarks>
+        public MetadataAttribute[] attributes
+        {
+            get { return _attributes.ToArray(); }
+            set
+            {
+                _attributes = value.ToList();
+                AttributeNames.Clear();
+                AttributesDictionary.Clear();
+                if (_attributes != null)
+                {
+                    foreach (var attr in _attributes)
+                    {
+                        AttributeNames.Add(attr.trait_type);
+                        AttributesDictionary[attr.trait_type] = attr;
+                    }
+                }
+            }
+        }
+        private List<MetadataAttribute> _attributes = new List<MetadataAttribute>();
+        public HashSet<string> AttributeNames = new HashSet<string>();
+        public Dictionary<string, MetadataAttribute> AttributesDictionary = new Dictionary<string, MetadataAttribute>();
         /// <summary>
         /// the collection which this NFT belongs to
         /// </summary>
         public Collection collection { get; set; }
+        /// <summary>
+        /// adds an attribute to the collection
+        /// </summary>
+        /// <param name="attribute"></param>
+        public void AddAttribute(MetadataAttribute attribute)
+        {
+            _attributes.Add(attribute);
+            AttributeNames.Add(attribute.trait_type);
+            AttributesDictionary[attribute.trait_type] = attribute;
+        }
+        /// <summary>
+        /// removes an attribute from the collection
+        /// </summary>
+        /// <param name="attribute"></param>
+        public void RemoveAttribute(MetadataAttribute attribute)
+        {
+            _attributes.Remove(attribute);
+            AttributeNames.Remove(attribute.trait_type);
+            AttributesDictionary.Remove(attribute.trait_type);
+        }
+        /// <summary>
+        /// updates the first instance of a given attribute in the collection
+        /// </summary>
+        /// <remarks>
+        /// either edit the reference or make sure the attribute is unique
+        /// </remarks>
+        /// <param name="attribute"></param>
+        public void UpdateAttribute(MetadataAttribute attribute)
+        {
+            var index = _attributes.FindIndex(a => a.trait_type == attribute.trait_type);
+            if (index != -1)
+            {
+                attributes[index] = attribute;
+                AttributesDictionary[attribute.trait_type] = attribute;
+            }
+        }
         /// <summary>
         /// Saves the Metadata to the specified path as json file
         /// </summary>
